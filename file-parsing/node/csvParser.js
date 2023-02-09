57ecc5d6d4f86fe0f963d53691ca5821f82191ae
Thor Find Andersen <thorfa4444@gmail.com>
2023-02-09 22:44:46 +0100

    Change for Assignment 1: changed to new CSV parser algorithmc for node

--- file-parsing/node/csvParser.js
+++ file-parsing/node/csvParser.js
@@ -1,18 +1,3 @@
-const fs = require("fs");
+const csv = require('csvtojson');
 const path = require("path");
-const { parse } = require("csv-parse");
-const inputString = fs.readFileSync(path.resolve(process.argv[2])).toString();
-const values = [];
-const parser = parse({columns: true, cast: true});
-parser.on('readable', () => {
-    let value;
-    console.log("Its readable");
-    while ((value = parser.read()) !== null){
-        values.push(value);
-        console.log(value);
-    }
-});
-parser.on('error', (error) => console.error(error.message));
-parser.on('end', () => console.log(values));
-parser.write(inputString);
-parser.end();
+csv().fromFile(path.resolve(process.argv[2])).then((jsonObj) => console.log(jsonObj[0]));
