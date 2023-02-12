d739ff8c7c0857c9d1af8b27386c4ea4b7ff7945
Thor Find Andersen <thorfa4444@gmail.com>
2023-02-12 23:07:55 +0100

    Change to Assignment 1: Fixed XML parser for python

--- file-parsing/python/xmlParser.py
+++ file-parsing/python/xmlParser.py
@@ -1,7 +1,7 @@
 import sys
 import xmltodict
 
-with open(o, 'r', encoding='utf-8') as file:
+with open(sys.argv[1], 'r', encoding='utf-8') as file:
     input = file.read()
 
 output = xmltodict.parse(input)
