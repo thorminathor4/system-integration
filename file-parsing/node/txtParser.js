fea48fc51fd1bbb6fe27f5627c8afee54aaf4e1f
Thor Find Andersen <thorfa4444@gmail.com>
2023-02-12 22:27:05 +0100

    Added to Assignment 1: Added TXT parser for node

---
+++ file-parsing/node/txtParser.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const path = require("path");
+const input = fs.readFileSync(path.resolve(process.argv[2])).toString();
+const output = {};
+input.split("\n").map(x => {
+    let {0: key, 1: val} = x.split("=");
+    if(val.includes(",")) val = val.split(",");
+    output[key] = val;
+});
+
+console.log(output);
